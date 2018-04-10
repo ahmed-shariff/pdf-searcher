6731c3728709f5026e17e3335e485212a9b11e45
Ahmed Shariff <ahmed.shariff@rocketmail.com>
2018-04-10 13:58:46 +0530

    bug fix for root directory not being scaned

--- pdf-searcher.lisp
+++ pdf-searcher.lisp
@@ -22,6 +22,7 @@
 Searches all pdf files recrursively under the root-pathname provided, and returns the list of files which has the search-string specified. The second value returned is a list of files which was determined to be unreadable. Uses pdftotext linux tool.
 "
   (labels ((collect-directories (unchecked-directories directory-list)
+	     (print unchecked-directories)
 	     (if (not (car unchecked-directories))
 		 directory-list
 		 (collect-directories (append (cdr unchecked-directories)
@@ -29,44 +30,45 @@ Searches all pdf files recrursively under the root-pathname provided, and return
 								      (directory-namestring (car unchecked-directories))
 								      "*")))
 				      (append directory-list (list (car unchecked-directories)))))))
-    (values-list
-     (loop for pdf-file in (apply #'append
-				  (loop for pdf-dir in (collect-directories
-							(if (wild-pathname-p root-pathname)
-							    (directory root-pathname)
-							    (directory (concatenate
-									'string
-									(directory-namestring
-									 (car (directory root-pathname)))
-									"*")))
-							'())
-					collect (directory
-						 (concatenate 'string
-							      (directory-namestring pdf-dir)
-							      "*.pdf"))))
+    (let ((root-pathname (remove #\/ root-pathname :from-end t :count 1 :start (- (length root-pathname) 1))))
+      (values-list
+       (loop for pdf-file in (apply #'append
+				    (loop for pdf-dir in (collect-directories
+							  (if (wild-pathname-p root-pathname)
+							      (directory root-pathname)
+							      (directory (concatenate
+									  'string
+									  (directory-namestring
+									   (car (directory root-pathname)))
+									  "/*")))
+							  (list root-pathname))
+					  collect (directory
+						   (concatenate 'string
+								(directory-namestring pdf-dir)
+								"*.pdf"))))
 					;for i from 0 upto 1
-	   ;do (print (pathname-directory pdf-file))
-	   if (scan
-	       (create-scanner (list :sequence
-				     :case-insensitive-p
-				     search-string
-				     :case-insensitive-p))
-	       (run/s (concatenate 'string
-				   "pdftotext \""
-				   (namestring pdf-file)
-				   "\" -")
-		      :on-error nil))
-	     collect (namestring pdf-file) into found
-	   else if (null (scan
-			  (create-scanner (list :sequence
-						:case-insensitive-p
-						(list :alternation "abstract" "reference")
-						:case-insensitive-p))
-			  (run/s (concatenate 'string
-					      "pdftotext \""
-					      (namestring pdf-file)
-					      "\" -")
-				 :on-error nil)))
-		  collect (namestring pdf-file) into unreadable
-	   finally
-	      (return (list found unreadable))))))
+	     do (print pdf-file)
+	     if (scan
+		 (create-scanner (list :sequence
+				       :case-insensitive-p
+				       search-string
+				       :case-insensitive-p))
+		 (run/s (concatenate 'string
+				     "pdftotext \""
+				     (namestring pdf-file)
+				     "\" -")
+			:on-error nil))
+	       collect (namestring pdf-file) into found
+	     else if (null (scan
+			    (create-scanner (list :sequence
+						  :case-insensitive-p
+						  (list :alternation "abstract" "reference")
+						  :case-insensitive-p))
+			    (run/s (concatenate 'string
+						"pdftotext \""
+						(namestring pdf-file)
+						"\" -")
+				   :on-error nil)))
+		    collect (namestring pdf-file) into unreadable
+	     finally
+		(return (list found unreadable)))))))
